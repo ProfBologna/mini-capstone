53f4ef52b371673a87ed2ea282784b5cb9146f1f
ProfBologna <sam.aporia@gmail.com>
2017-08-29 20:18:28 -0500

    Added viewable webpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get 'great_old_ones_url' => 'pages#great_old_ones_action'
 end
