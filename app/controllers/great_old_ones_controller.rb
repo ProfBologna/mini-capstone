a44fe5df349e1a236e9b0e0479002c45b93bd0c9
ProfBologna <sam.aporia@gmail.com>
2017-08-31 21:29:54 -0500

    Updated page to have create options

--- app/controllers/great_old_ones_controller.rb
+++ app/controllers/great_old_ones_controller.rb
@@ -1,6 +1,27 @@
 class GreatOldOnesController < ApplicationController
-  def great_old_ones_action
-    @great_old_one = GreatOldOne.all
-    render 'great_old_ones.html.erb'
+  
+  def index
+    @goo = GreatOldOne.all
   end
+
+  def new
+
+  end
+
+  def create
+    great_old_one = GreatOldOne.new(
+                                    name: params[:name],
+                                    description: params[:description],
+                                    price: params[:price],
+                                    image: params[:image]
+                                    )
+
+    great_old_one.save
+  end
+
+  def show
+    @great_old_one = GreatOldOne.find(params[:id])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-get 'great_old_ones_url' => 'great_old_ones#great_old_ones_action'
+get '/' => 'great_old_ones#index'
+get '/great_old_ones' => 'great_old_ones#index'
+
+get '/great_old_ones/new' => 'great_old_ones#new'
+post '/great_old_ones' => 'great_old_ones#create'
+
+get '/great_old_ones/:id' => 'great_old_ones#show'
+
 end
