4250f68be9798c8b649819ebadd106e07a57d4ec
ProfBologna <sam.aporia@gmail.com>
2017-09-05 21:21:47 -0500

    Lots of udates to mini capstone, including redirects and alert messages. Destroy and edit buttons added. Create new button added.

--- app/controllers/great_old_ones_controller.rb
+++ app/controllers/great_old_ones_controller.rb
@@ -17,10 +17,38 @@ class GreatOldOnesController < ApplicationController
                                     )
 
     great_old_one.save
+    flash[:success] = "A cosmic horror has been summoned from the void..."
+    redirect_to "/great_old_ones/#{ great_old_one.id }"
   end
 
   def show
     @great_old_one = GreatOldOne.find(params[:id])
+  end
+
+  def edit
+    @great_old_one = GreatOldOne.find(params[:id])
+  end
+
+  def update
+    goo = GreatOldOne.find(params[:id])
+
+    goo.assign_attributes(
+                          name: params[:name],
+                          price: params[:price],
+                          image: params[:image],
+                          description: params[:description]
+                          )
+
+    goo.save
+    flash[:success] = "Rituals unknown seem to have altered our perception and understanding of this cosmic horror."
+    redirect_to "/great_old_ones/#{ goo.id }"
+  end
+
+  def destroy
+    goo = GreatOldOne.find(params[:id])
+    goo.destroy
+    flash[:danger] = "You have successfully banished something outside of time and nature... but how?"
+    redirect_to "/great_old_ones"
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  
 get '/' => 'great_old_ones#index'
 get '/great_old_ones' => 'great_old_ones#index'
-
 get '/great_old_ones/new' => 'great_old_ones#new'
 post '/great_old_ones' => 'great_old_ones#create'
-
 get '/great_old_ones/:id' => 'great_old_ones#show'
+get 'great_old_ones/:id/edit' => 'great_old_ones#edit'
+patch 'great_old_ones/:id' => 'great_old_ones#update'
+delete 'great_old_ones/:id' => 'great_old_ones#destroy'
 
 end
