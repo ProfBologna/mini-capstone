cf63d8ad9010d52ff07a3e1df5711ad9894d8387
ProfBologna <sam.aporia@gmail.com>
2017-09-06 21:31:45 -0500

    Updated application recond

--- app/models/great_old_one.rb
+++ app/models/great_old_one.rb
@@ -1,2 +1,20 @@
 class GreatOldOne < ApplicationRecord
+
+  def sale_message
+    sales_message = ""
+
+    sales_message = "Discount horror!" if price < 600
+    sales_message = "Everyday value!" if price > 600
+
+    sales_message
+  end
+
+  def tax
+    tax = price * 0.09
+  end
+
+  def total
+    total = price + tax
+  end
+
 end
